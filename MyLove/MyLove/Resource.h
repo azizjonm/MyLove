9d45b71c0e3bb904aa7f740dfdba08e9e6211bab
Marvin <niklaus520@aol.com>
2014-05-28 22:28:59 +0800

    add something
    add file manager, keyboard logger, screen spy, cmd shell, sound set

--- MyLove/MyLove/Resource.h
+++ MyLove/MyLove/Resource.h
@@ -4,11 +4,16 @@
 //
 #define IDD_ABOUTBOX                    100
 #define IDP_OLE_INIT_FAILED             100
+#define IDD_SOUNDSET                    101
 #define IDP_SOCKETS_INIT_FAILED         104
 #define IDR_MAINFRAME                   128
 #define IDR_MyLoveTYPE                  130
 #define ID_WINDOW_MANAGER               131
 #define IDD_FILE                        311
+#define IDD_FILE_MANAGER                311
+#define IDD_KEYBOARD_LOGGER             312
+#define IDD_SCREENSPY                   313
+#define IDD_SHELL                       314
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
 #define IDC_LOCAL_PATH                  1003
@@ -16,14 +21,22 @@
 #define IDC_REMOTE_PATH                 1005
 #define IDC_LIST2                       1006
 #define IDC_LIST_REMOTE                 1006
+#define IDC_EDIT                        1007
+#define IDC_USEORNOT                    1013
+#define IDC_DEFAULT                     1014
+#define IDC_IFONLINE                    1017
+#define IDC_ONLINE                      1018
+#define IDC_OFFLINE                     1019
+#define IDC_BUTTON4                     1020
+#define IDC_IFOFFLINE                   1020
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        318
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
