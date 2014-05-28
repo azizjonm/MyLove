7a9f83f9629cff5aedb9ecee2e1c972f1bfc5d03
Marvin <niklaus520@aol.com>
2014-05-28 21:16:26 +0800

    haha

--- MyLove/MyLove/Resource.h
+++ MyLove/MyLove/Resource.h
@@ -8,10 +8,14 @@
 #define IDR_MAINFRAME                   128
 #define IDR_MyLoveTYPE                  130
 #define ID_WINDOW_MANAGER               131
-#define IDD_DIALOG1                     311
-#define IDCANCEL                        1000
+#define IDD_FILE                        311
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
+#define IDC_LOCAL_PATH                  1003
+#define IDC_LIST_LOCAL                  1004
+#define IDC_REMOTE_PATH                 1005
+#define IDC_LIST2                       1006
+#define IDC_LIST_REMOTE                 1006
 
 // Next default values for new objects
 // 
@@ -19,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
