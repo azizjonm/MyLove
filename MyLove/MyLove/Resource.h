a767c4484b462b3a8835300371d2e26e3f76e4d9
Marvin <niklaus520@aol.com>
2014-06-01 00:46:10 +0800

    MyLove

--- MyLove/MyLove/Resource.h
+++ MyLove/MyLove/Resource.h
@@ -14,29 +14,63 @@
 #define IDD_KEYBOARD_LOGGER             312
 #define IDD_SCREENSPY                   313
 #define IDD_SHELL                       314
+#define IDD_SYSTEM                      318
+#define IDD_TRANSFERMODE_DLG            319
+#define IDD_UPDATE_IP                   320
+#define IDD_UPDATE_SERVER               321
+#define IDD_FORMVIEW                    322
+#define IDD_WEBCAM                      323
+#define IDD_DIALOG2                     324
+#define IDC_REMOTE_HOST                 324
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
 #define IDC_LOCAL_PATH                  1003
+#define IDC_EXIT                        1003
 #define IDC_LIST_LOCAL                  1004
 #define IDC_REMOTE_PATH                 1005
 #define IDC_LIST2                       1006
 #define IDC_LIST_REMOTE                 1006
+#define IDC_LIST_WINDOWS                1006
 #define IDC_EDIT                        1007
 #define IDC_USEORNOT                    1013
 #define IDC_DEFAULT                     1014
 #define IDC_IFONLINE                    1017
 #define IDC_ONLINE                      1018
 #define IDC_OFFLINE                     1019
-#define IDC_BUTTON4                     1020
 #define IDC_IFOFFLINE                   1020
+#define IDC_SET                         1021
+#define IDC_BUTTON5                     1022
+#define IDC_IFSCUESS                    1022
+#define IDC_SKIP                        1022
+#define IDC_LINE                        1023
+#define IDC_SPLASH                      1024
+#define IDC_DISABLE_POPTIPS             1025
+#define IDC_CONNECT_MAX                 1026
+#define IDC_PASS                        1027
+#define IDC_LISTEN_PORT                 1028
+#define IDC_CONNECT_AUTO                1029
+#define IDC_CHANGE_PASS                 1030
+#define IDC_RESETPORT                   1031
+#define IDC_TAB1                        1033
+#define IDC_LIST_DIALUPASS              1035
+#define IDC_LIST_PROCESS                1038
+#define IDC_ADDITION                    1039
+#define IDC_ADDITION_ALL                1040
+#define IDC_SKIP_ALL                    1041
+#define IDC_CANCEL                      1042
+#define IDC_OVERWRITE                   1043
+#define IDC_OVERWRITE_ALL               1044
+#define IDC_UPDATE_IP_TAB               1045
+#define IDC_UPDATE_SERVER_TAB           1046
+#define IDD_UPDATE_SHOW                 1047
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        318
+#define _APS_NEXT_RESOURCE_VALUE        325
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1048
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
